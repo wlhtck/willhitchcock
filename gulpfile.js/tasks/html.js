3e07c9eceec37f012e1910eed96403c4e083b3ca
William Hitchcock <william.hitchcock@mrm-mccann.com>
2017-03-27 11:07:01 -0400

    First commit, some new gulp tasks WIP

---
+++ gulpfile.js/tasks/html.js
@@ -0,0 +1,26 @@
+var config       = require('../config')
+if(!config.tasks.html) return
+
+var browserSync  = require('browser-sync')
+var gulp         = require('gulp')
+var gulpif       = require('gulp-if')
+var htmlmin      = require('gulp-htmlmin')
+var path         = require('path')
+
+var paths = {
+  src: [path.join(config.root.src, config.tasks.html.src, '/**/*.{' + config.tasks.html.extensions + '}')],
+  dest: path.join(config.root.dest, config.tasks.html.dest),
+}
+
+var htmlTask = function() {
+  console.log(paths.src, paths.dest)
+
+  return gulp.src(paths.src)
+    .pipe(gulpif(global.production, htmlmin(config.tasks.html.htmlmin)))
+    .pipe(gulp.dest(paths.dest))
+    .on('end', browserSync.reload)
+
+}
+
+gulp.task('html', htmlTask)
+module.exports = htmlTask
